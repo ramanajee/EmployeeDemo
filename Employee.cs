e33db43740e440e7d72a31ace85e8871cb599124
Ramanajee <ramanajee040@gmail.com>
2016-04-16 22:31:09 +0530

    Initial

---
+++ Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDemo
+{
+    // Class
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+    //Structs
+    public struct Address
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PIN { get; set; }
+    }
+    //Interface
+    public interface IEmployeeContext<T> where T : class
+    {
+        void Add(T entity);
+        ICollection<T> Get();
+        void Delete(T entity);
+        T Get(int id);
+    }
+    // Interface Implementation
+    public class EmployeeContext<T>:IEmployeeContext<T> where T : class
+    {
+        List<T> db = null;
+
+        public EmployeeContext(List<T> dataList)
+        {
+            db = dataList;
+        }
+
+        public void Add(T entity)
+        {
+            db.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            db.Remove(entity);
+        }
+
+        public T Get(int id)
+        {
+            return db.ElementAt<T>(id-1);
+        }
+        public ICollection<T> Get()
+        {
+            return db;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,62 @@ namespace EmployeeDemo
 {
     class Program
     {
+       public static List<Employee> data = new List<Employee> {
+            new Employee { Id = 1 , Name = "Ramanajee" , Email="ramanajee@qualminds.com"},
+            new Employee { Id = 2 , Name ="ram" ,  Email = "ram@live.in" }
+            };
+       private EmployeeContext<Employee> context = new EmployeeContext<Employee>(data);
+
+        public ICollection<Employee> Get()
+        {
+            return context.Get();
+        }
+        // Method Overloading
+        public Employee GetById(int id)
+        {
+            return context.Get(id);
+        }
+
+        public void Delete(int id)
+        {
+            var employee = context.Get(id);
+            context.Delete(employee);
+        }
+
+        public void Add(Employee employee)
+        {
+            context.Add(employee);
+        }
         static void Main(string[] args)
         {
+            Program p = new Program();
+            Address address = new Address();
+            var data = p.Get();
+            Console.WriteLine("============================================================================");
+            foreach (var item in data)
+            {
+                Console.WriteLine("Id:" + item.Id + " Name:" + item.Name + " Email:" + item.Email);
+            }
+
+            Console.WriteLine("============================================================================");
+            Console.WriteLine("Enter employee Address:\n");
+            Console.WriteLine("Enter Id:");
+            address.Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter EmployeeId:");
+            address.EmployeeId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Street:");
+            address.Street = Console.ReadLine();
+            Console.WriteLine("Enter City:");
+            address.City = Console.ReadLine();
+            Console.WriteLine("Enter Country:");
+            address.Country = Console.ReadLine();
+            Console.WriteLine("Enter PIN:");
+            address.PIN = Console.ReadLine();
+            Console.WriteLine("===============================================================================");
+            Console.WriteLine("Employee Full Details");
+            var employee = p.GetById(address.EmployeeId);
+            Console.WriteLine("Id:" + employee.Id + " Name:" + employee.Name + " Email:" + employee.Email);
+            Console.WriteLine("Street:" + address.Street + ", City:" + address.City + ", Country:" + address.Country + ", PIN:" + address.PIN);
             Console.ReadLine();
         }
     }
